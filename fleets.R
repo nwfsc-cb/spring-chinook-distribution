6d89bdb84affaf16e321478f52ed1c6d4153e05d
genoa sullaway <genoa.sullaway@noaa.gov>
2018-10-31 14:13:06 -0700

    update
    script update added highseas plots with free_y

--- fleets.R
+++ fleets.R
@@ -28,27 +28,44 @@ dat_recovery_loc$fishery <- as.numeric(dat_recovery_loc$fishery)
 
             ############# HIGH SEAS #############
 highseas <- dat_recovery_loc %>%
-#shouldd just do this to dat recovery loc original doc and not have to keep doing it everytime
     filter(fishery %in% c(80, 800, 802, 803, 804, 805, 81, 812, 82, 83, 84, 85, 87, 88, 89))
 
 #rename highseas
+#based on RMIS Documentation 
 highseas$fishery_name <- recode_factor(highseas$fishery, '80' = "Hake Trawl At Sea",
-                                             '800' = "Hake Trawl Shoreside",
-                                             '802' = "Rockfish Trawl",
-                                             '803'="Groundfish Trawl",
-                                             '804'="Sablefish Fixed Gear",
-                                             '805'="Nearshore Groundfish",
-                                             '81'="Groundfish Observer",
-                                             '812'="Rockfish",
-                                             '82'="Groundfish Observer",
-                                             '83'="Foreign Research Vessels",
-                                             '84'="Foreign Mothership",
-                                             '85'="Ocean Trawl By-catch",
-                                             '87'="Squid Gillnet By-catch", 
-                                             '88'="Juv Sampling",
-                                             '89'="Other")
-                                      
+                                       '800' = "Hake Trawl Shoreside",
+                                       '802' = "Rockfish Trawl",
+                                       '803'="Groundfish Trawl",
+                                       '804'="Sablefish Fixed Gear",
+                                       '805'="Nearshore Groundfish",
+                                       '81'="Groundfish Observer",
+                                       '812'="Rockfish Fishery",
+                                       '82'="Groundfish Observer",
+                                       '83'="Foreign Research Vessels",
+                                       '84'="Foreign Mothership",
+                                       '85'="Ocean Trawl By-catch",
+                                       '87'="Squid Gillnet By-catch", 
+                                       '88'="Juv Sampling",
+                                       '89'="Other")
 
+#sort high seas to get recovery #s by year and fishery to look at available data through time (NOT space)
+highseas_group <- highseas %>%
+  group_by(rec_year, fishery_name) 
+  highseas_count=count(highseas_group, fishery_name) 
+  highseas_spread <- highseas_count %>%
+    spread(fishery_name, n)
+highseas_spread[is.na(highseas_spread)] <- 0
+
+hs_bar <- ggplot(highseas_count, aes(x=rec_year, y=n)) +
+  geom_bar(position="dodge", stat="identity") +
+  facet_grid(fishery_name ~ .,scales = "free_y",labeller=label_wrap_gen(width=.1)) +
+  ggtitle('High Seas Recoveries') +
+  labs(x='Recovery Year', y= 'Instance of Fish recovery')
+hs_bar
+
+write.csv(highseas_spread, "highseas.csv")
+
+########### Recoveries through time and space
 #base map
 #had to take the world map and parce it down by filtering out lats and longs I didnt want since subregions were not in data set
 world <- map_data("world")
